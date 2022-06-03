00a004feb53eff03090935eba4d6ef80898e16ff
Vsaty <583429957@qq.com>
2022-06-04 03:29:30 +0800

    菜品分页查询

--- src/main/java/edu/xjnu/MoreBowl/controller/CategoryController.java
+++ src/main/java/edu/xjnu/MoreBowl/controller/CategoryController.java
@@ -1,5 +1,8 @@
 package edu.xjnu.MoreBowl.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import edu.xjnu.MoreBowl.common.R;
 import edu.xjnu.MoreBowl.entity.Category;
 import edu.xjnu.MoreBowl.service.CategoryService;
@@ -18,11 +21,34 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    /**
+     * 添加分类
+     * @return
+     */
     @PostMapping
     public R<String> save(@RequestBody Category category) {
         log.info(category.toString());
         categoryService.save(category);
         return  R.success("添加成功");
+    }
+
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<IPage> page(Integer page,Integer pageSize) {
+        // 分页构造器
+        IPage<Category> pageInfo = new Page<>(page, pageSize);
+        // 条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        // 添加排序条件, 根据sort进行排序
+        queryWrapper.orderByAsc(Category::getSort);
+        // 分页查询
+        categoryService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
     }
 }
 
--- src/main/java/edu/xjnu/MoreBowl/entity/Category.java
+++ src/main/java/edu/xjnu/MoreBowl/entity/Category.java
@@ -55,6 +55,7 @@ public class Category implements Serializable {
 
 
     //是否删除
+    @TableField(select = false)
     private Integer isDeleted;
 
 }
