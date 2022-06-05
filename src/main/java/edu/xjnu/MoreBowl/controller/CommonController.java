c31a89779f8aaede09ff989928bf47a568fcbdf0
Vsaty <583429957@qq.com>
2022-06-05 17:38:08 +0800

    debug: 给回显图片的路径增加@GetMapping注解

--- src/main/java/edu/xjnu/MoreBowl/controller/CommonController.java
+++ src/main/java/edu/xjnu/MoreBowl/controller/CommonController.java
@@ -3,6 +3,7 @@ package edu.xjnu.MoreBowl.controller;
 import edu.xjnu.MoreBowl.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,7 @@ public class CommonController {
         return R.success(fileName);
     }
 
+    @GetMapping("download")
     public void download(String name, HttpServletResponse response) {
         try {
             // 输入流, 通过输入流获取文件内容
